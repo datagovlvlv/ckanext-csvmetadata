3d0a940d05a95b66cd1a8e40da008b6057b48c19
CRImier <crimier@yandex.ru>
2017-04-05 04:30:34 +0300

    WIP - SSD glitching

--- ckanext/csvmetadata/plugin.py
+++ ckanext/csvmetadata/plugin.py
@@ -7,6 +7,7 @@ import logging
 import requests
 from StringIO import StringIO
 from collections import OrderedDict
+from ast import literal_eval as l_eval
 
 
 from ckanapi import LocalCKAN
@@ -72,86 +73,13 @@ class ResourceCSVController(base.BaseController):
             element["required"] = element.pop("required") if "required" in element else False
         return schema
 
-    def form_to_csvw(self, form_data):
-        csv_headers_str = form_data.pop("csv_headers")
-        csv_headers = csv_headers_str.split('\",\"')
-
-        #Dictionary to store CSVW data
-        csvw_json_data = OrderedDict()
-        csvw_json_data["@context"] = "http://www.w3.org/ns/csvw"
-        csvw_json_data["@type"] = "Table"
-        #Creating a dictionary for each CSV header so that we can stuff data from form to those dictionaries
-        schema = {"columns":[dict() for i in range(len(csv_headers))]}
-
-        form_elements = form_data.keys()
-        form_elements.sort()
-        for key in form_elements:
-            try:
-                header_num_str, form_field_name = key.split("-", 1)
-                header_num = int(header_num_str)
-            except:
-                print("Error while parsing key {}".format(key))
-            else:
-                schema["columns"][header_num][form_field_name] = form_data[key]
-        #Working around checkboxes - they'll be in the form data if set, but as '"checkbox_name":""' instead of '"checkbox_name"=True'
-        form_schema = self.get_form_schema()
-        checkbox_ids = [element["name"] for element in form_schema["form_fields"] if element["preset"] == "checkbox"]
-        print(checkbox_ids)
-        for column in schema["columns"]:
-            for checkbox_id in checkbox_ids:
-                if checkbox_id in column.keys():
-                    column["checkbox_id"] = True
-
-        #Adding created schema to CSVW dictionary
-        csvw_json_data["tableSchema"] = schema
-        #Turn OrderedDict into JSON
-        csvw_json_string_data = json.dumps(csvw_json_data, indent=2)
-        return csvw_json_string_data
-
-    def resource_csv(self, id, resource_id):
-        contents = p.toolkit.get_action('resource_show')(None, {'id': resource_id})
-
-        form_schema = self.get_form_schema()
-        resource_url = contents["url"]
-        
-        #Checking if we received a POST request - 
-        #then we need to create JSON from received data and upload it
-        if toolkit.request.method == 'POST':
-            #Loading data from form
-            form_data = p.toolkit.request.POST
-            csvw_string = self.form_to_csvw(dict(form_data))
-            print(csvw_string)
-            io_object = StringIO(csvw_string)
-
-            #monkeypatching because ckanapi gets filename from descriptor
-            io_object.name = "'{}'_metadata.json".format(id)
-            ckan_api.action.resource_create(package_id=id, name=io_object.name, url="I don't know how to figure out the URL", upload=io_object)
-            #Successfully uploaded, now redirecting to the package contents page to show that JSON file was created successfully
-            core_helpers.redirect_to(
-                controller='package',
-                action='read',
-                id=id
-            )
-
-        #Assuming GET method
-
-        #This data is, apparently, needed so that resource data is properly included in the template?
-        try:
-            toolkit.c.pkg_dict = p.toolkit.get_action('package_show')(
-                None, {'id': id}
-            )
-            toolkit.c.resource = p.toolkit.get_action('resource_show')(
-                None, {'id': resource_id}
-            )
-        except (logic.NotFound, logic.NotAuthorized):
-            base.abort(404, _('Resource not found'))
-
-        #Getting CSV from the resource url
+    def get_csv_sample(self, csv_url):
         status = "ok"
         csv_headers = []
-
+        csv_info = {"delimiter":"", "encoding":"TOFIX", "quotechar":""}
+        
         try:
-            req = requests.get(resource_url, timeout=10, stream=True)
+            req = requests.get(csv_url, timeout=10, stream=True)
         except:
             status = "url_fail"
         else:
@@ -177,13 +105,204 @@ class ResourceCSVController(base.BaseController):
                     delimiter = str(dialect.delimiter)
                     quotechar = str(dialect.quotechar)
                     csv_headers = csv.reader([csv_headers_str], delimiter=delimiter, quotechar=quotechar).next()
+                    csv_info["delimiter"] = delimiter
+                    csv_info["quotechar"] = quotechar
             else:
                 status = "http_error_{}".format(req.status_code)
+        return status, csv_headers, csv_info
+
+    def fetch_json_return_values(self, json_url):
+        req = requests.get(json_url)
+        json_dict = json.loads(req.text)
+        return json_dict
+
+    def eval_remove_from_form(self, form_data, data_name):
+        data_str = form_data.pop(data_name)
+        return l_eval(data_str)
+
+    def form_to_csvw(self, form_data):
+        #Assumes info about current resource is in toolkit.c.pkg_dict
+        #and info about current package is in toolkit.c.resource
+
+        #csv headers and csv info dict are passed along with form data, removing them from the form data
+        csv_info = self.eval_remove_from_form(form_data, "csv_info")
+        csv_headers = self.eval_remove_from_form(form_data, "csv_headers")
+
+        #getting data about organization
+        org_data = toolkit.c.pkg_dict["organization"]
+        org_url = "TOFIX"
+
+        #Dictionary to store CSVW data
+        csvw_json_data = OrderedDict()
+        csvw_json_data["@context"] = ["http://www.w3.org/ns/csvw", {"@language":"lv"}]
+        #csvw_json_data["@type"] = "Table"
+        csvw_json_data["url"] = toolkit.c.resource["url"]
+        csvw_json_data["dialect"] = csv_info
+        csvw_json_data["dc:title"] = toolkit.c.pkg_dict["title"]
+        csvw_json_data["dcat:keyword"] = [tag["name"] for tag in toolkit.c.pkg_dict["tags"]] if "tags" in toolkit.c.pkg_dict else []
+        print(org_data["title"])
+        csvw_json_data["dc:publisher"] = {"schema:name":org_data["title"],
+                                          "schema:url":org_url}
+        csvw_json_data["dc:license"] = {"@id":toolkit.c.pkg_dict["license_url"]}
+        csvw_json_data["dc:issued"] = {"@value":toolkit.c.resource["created"].split("T")[0], "@type":"xsd:date"}
+        if toolkit.c.resource["last_modified"]:
+            modified_date = toolkit.c.resource["last_modified"].split("T")[0]
+        else:
+            modified_date = csvw_json_data["dc:issued"]["@value"]
+        csvw_json_data["dc:modified"] = {"@value":modified_date, "@type":"xsd:date"}
         
-        print(status)
-        print(csv_headers)
+        #Creating a dictionary for each CSV header so that we can stuff data from form to those dictionaries
+        schema = {"columns":[dict() for i in range(len(csv_headers))]}
+
+        #form field names come in "{header_num}-{form_field_name}"
+        form_elements = form_data.keys()
+        form_elements.sort()
+        for key in form_elements:
+            try:
+                header_num_str, form_field_name = key.split("-", 1)
+                header_num = int(header_num_str)
+            except:
+                print("Error while parsing key {}".format(key))
+            else:
+                schema["columns"][header_num][form_field_name] = form_data[key]
+        
+        #Working around checkboxes - they'll be in the form data if set, but as '"checkbox_name":""' instead of '"checkbox_name"=True'
+        form_schema = self.get_form_schema()
+        checkbox_ids = [element["name"] for element in form_schema["form_fields"] if element["preset"] == "checkbox"]
+        for column in schema["columns"]:
+            for checkbox_id in checkbox_ids:
+                column[checkbox_id] =  checkbox_id in column.keys()
+
+        #Now, re-formatting the resulting schema dictionary to conform with the specification
+        for column in schema["columns"]:
+            #dict is passed by reference, so we don't have to replace it after changing its contents
+            base = column.pop("datatype")
+            length = column.pop("length")
+            column["datatype"] = {"base":base, "length":length}
+            #Now need to process primary and secondary keys
+            resource = column.pop("resource")
+            columnReference = column.pop("columnReference")
+            foreign_keys = column.pop("foreignKeys")
+            if foreign_keys:
+                column["foreignKeys"] = [{ "reference" : {"resource":resource, "columnReference":columnReference}}]
+            else:
+                column["foreignKeys"] = []
+
+        #Adding created schema to CSVW dictionary
+        csvw_json_data["tableSchema"] = schema
+        #Turn OrderedDict into JSON
+        csvw_json_string_data = json.dumps(csvw_json_data, indent=2)
+        return csvw_json_string_data
+
+    def find_existing_json_for_resource(self, resources, json_filename):
+        """ 
+            Function determines if CSVW file with a given filename already exists among resources.
+            If it does, returns URL to download that file and its resource ID. Otherwise, returns None and None.
+        """
+        json_url = None
+        json_res_id = None
+        #Going through resources to see if metadata JSON already exists
+        for resource in resources:
+            #If there are multiple versions with same filenames, selecting the last one in the list of resources
+            if self.filename_for_url(resource["url"]) == json_filename:
+                json_url = resource["url"]
+                json_res_id = resource["id"]
+                print(json_res_id)
+        return json_url, json_res_id
+
+    def filename_for_url(self, url):
+        """
+           Given an URL, returns its last part which, most likely, contains the resource's filename.
+        """
+        #(Ab)uses os.path.split, though url.rsplit() could also work.
+        return os.path.split(url)[1]
+
+    def make_json_filename(self, resource_filename):
+        """
+            Defines how the JSON file filename is constructed. 
+        """
+        #Changing this will change both creation of new files and matching to old files,
+        #so changing this format will break backwards compatibility 
+        #unless code that relies on is modified accordingly.
+        return "{}_metadata.json".format(resource_filename)
+
+    def resource_csv(self, id, resource_id):
+        """
+            Controller for "CSV metadata" page 
+        """
+        #Getting information about the package and resource
+        #It's necessary for the base template to render, and we can also use the data ourselves
+        try:
+            toolkit.c.pkg_dict = p.toolkit.get_action('package_show')(None, {'id': id})
+            toolkit.c.resource = p.toolkit.get_action('resource_show')(None, {'id': resource_id})
+        except (logic.NotFound, logic.NotAuthorized):
+            base.abort(404, _('Resource not found'))
+
+        #getting form schema to pass to template
+        form_schema = self.get_form_schema()
+        
+        #getting resource url, checking if it's good
+        resource_url = toolkit.c.resource["url"] if "url" in toolkit.c.resource else ""
+        if not resource_url:
+            #No URL shown or empty URL, failing early
+            return base.render('csvmetadata/resource_csv.html',
+                           extra_vars={'status':'url_fail'})
+
+        resource_filename = self.filename_for_url(resource_url)
+        #Info about other resources - for JSON file lookup
+        other_resources = [resource for resource in toolkit.c.pkg_dict["resources"] if resource["id"] != resource_id]
+        
+        #Checking if we're in a POST request - 
+        #then we need to create JSON from received data and upload it
+        if toolkit.request.method == 'POST':
+            #Loading data from form
+            form_data = p.toolkit.request.POST
+            csvw_string = self.form_to_csvw(dict(form_data))
+            print(csvw_string)
+            io_object = StringIO(csvw_string)
+            
+            filename = self.make_json_filename(resource_filename)
+            #monkeypatching because ckanapi gets filename from descriptor
+            io_object.name = filename
+
+            resource_name = filename
+            
+            _, json_resource_id = self.find_existing_json_for_resource(other_resources, self.make_json_filename(resource_filename))
+            if json_resource_id:
+                print("Updating resource")
+                ckan_api.action.resource_update(id=json_resource_id, url="", upload=io_object)
+            else:
+                print("Creating resource")
+                #ckan_api.action.resource_create(package_id=id, name=resource_name, url="", upload=io_object)
+            #Successfully uploaded, now redirecting to the package contents page to show that JSON file was created successfully
+            core_helpers.redirect_to(
+                controller='package',
+                action='read',
+                id=id
+            )
+
+        #POST request processing code didn't continue, assuming GET method
+        json_url, _ = self.find_existing_json_for_resource(other_resources, self.make_json_filename(resource_filename))
+        if json_url:
+            #Some kind of JSON URL is found, let's fetch it and get CSV header descriptions
+            values = {}
+            try:
+                json_dict = self.fetch_json_return_values(json_url)
+                values = json_dict["tableSchema"]["columns"]
+            except Exception as e:
+                import pdb; pdb.set_trace()
+                pass #JSON is either unfetchable or badly constructed, so we won't use it
+        
+        #Getting CSV from the resource url
+        status, csv_headers, csv_info = self.get_csv_sample(resource_url)
+
+        print(values)
         return base.render('csvmetadata/resource_csv.html',
-                           extra_vars={'status':status, 'csv_headers':csv_headers, 'schema':form_schema})
+                           extra_vars={'status':status, 
+                                       'csv_headers':csv_headers, 
+                                       'schema':form_schema, 
+                                       'values':values,
+                                       'csv_info':repr(csv_info)})
 
 
 class CSVMetadataPlugin(p.SingletonPlugin):
