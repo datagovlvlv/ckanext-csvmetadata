36abbd066ae44ca73f63d47139c39e884f5518b8
CRImier <crimier@yandex.ru>
2017-04-09 23:48:53 +0300

    Added "coloring required fields in pink" - untested

--- ckanext/csvmetadata/fanstatic/csvmetadata_required.js
+++ ckanext/csvmetadata/fanstatic/csvmetadata_required.js
@@ -1,10 +1,19 @@
 "use strict";
-
+	
 ckan.module('csvmetadata-required', function ($) {
   return {
     initialize: function () {
       this.el.submit( function (e) {
               window.form_valid = true;
+              $.each( $('[data-required="true"]'), 
+                  function (i, element) { 
+                        var value = $(element).val();
+                        var element_hidden = $(element).is(":hidden");
+                        if (!value && !element_hidden) {
+                            $(element).css({"background-color": "lightpink"});
+                        } 
+                  }
+              );
               $.each( $('[data-required="true"]'), 
                   function (i, element) { 
                         var value = $(element).val();
